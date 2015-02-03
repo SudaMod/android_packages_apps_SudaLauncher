797a08f72416a3bc7db1e65e5ac1a1ba40bf460b
emancebo <emancebo@cyngn.com>
2015-02-03 12:15:11 -0800

    Trebuchet: ensure preview animation is released after exiting TransitionEffectsFragment
    Change-Id: Id6253a6162ee47dd1a46886026f09c3adbdc7027
    (cherry picked from commit a426812f58b320c191ed8427d145966724949d67)

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -1396,6 +1396,7 @@ public class Launcher extends Activity
         }
         fragmentTransaction
                 .remove(mTransitionEffectsFragment).commit();
+        mTransitionEffectsFragment = null;
 
         mDarkPanel.setVisibility(View.VISIBLE);
         ObjectAnimator anim = ObjectAnimator.ofFloat(
