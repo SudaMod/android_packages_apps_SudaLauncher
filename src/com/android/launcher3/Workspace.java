8c2a229595c2fa8474ff5869b35cc5952f1a1858
Matt Garnes <matt@cyngn.com>
2015-05-12 18:02:32 -0700

    Do not reload settings for changing state to All Apps.
    When getStateChangeAnimation() is called, only reload settings if
    leaving overview mode.
    
    Change-Id: I9ee7330779b19bb73c1a30497065854ce9fc1162

--- src/com/android/launcher3/Workspace.java
+++ src/com/android/launcher3/Workspace.java
@@ -2308,9 +2308,6 @@ public class Workspace extends SmoothPagedView
             return null;
         }
 
-        // Check to see if new Settings need to be taken
-        reloadSettings();
-
         // Initialize animation arrays for the first time if necessary
         initAnimationArrays();
 
@@ -2340,6 +2337,11 @@ public class Workspace extends SmoothPagedView
         boolean allAppsToWorkspace = (stateIsNormalHidden && stateIsNormal);
         final boolean workspaceToOverview = (oldStateIsNormal && stateIsOverview);
         final boolean overviewToWorkspace = (oldStateIsOverview && stateIsNormal);
+
+        if (overviewToWorkspace || overviewToAllApps) {
+            // Check to see if new Settings need to be taken
+            reloadSettings();
+        }
 
         mNewScale = 1.0f;
 
